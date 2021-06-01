819c581636ec4d5aa873bec9dedfe02992c06e42
Johan Aerens <johan.aerens@securex.be>
2021-06-01 19:57:30 +0200

    first stable burger

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,13 +1,16 @@
-import React, {Component} from 'react';
+import React from "react";
+import classes from "./Burger.module.css";
+import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
-class Burger extends Component {
-    render() {
-        return (
-            <div>
+const burger = (props) => {
+    return (
+        <div className={classes.Burger}>
+            <BurgerIngredient type="bread-top"/>
+            <BurgerIngredient type="cheese"/>
+            <BurgerIngredient type="meat"/>
+            <BurgerIngredient type="bread-bottom"/>
+        </div>
+    );
+};
 
-            </div>
-        );
-    }
-}
-
-export default Burger;
+export default burger;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,13 +1,15 @@
 import React, {Component, Fragment} from "react";
+import Burger from "../../components/Burger/Burger"
 
 class BurgerBuilder extends Component {
     render() {
         return (
             <Fragment>
-                <div>Burger</div>
+                <div><Burger/></div>
                 <div>Build Controls</div>
             </Fragment>
         );
     }
 }
+
 export default BurgerBuilder;
