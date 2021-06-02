42494a9eb2bd619662f00055ebaf8a4eccdf7e82
Johan Aerens <johan.aerens@securex.be>
2021-06-02 18:29:50 +0200

    tweak omdat modal enkel niet altijd zichtbaar is en dus maw niet moet geupdate worden

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,28 +1,30 @@
-import React, {Fragment} from "react";
+import React, {Component, Fragment} from "react";
 import Button from "../../UI/Button/Button"
 
-const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients)
-        .map(igKey => {
-            return (
-                <li key={igKey}>
-                    <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
-                </li>
-            )
-        })
-
-    return (
-        <Fragment>
-            <h3>Your Order</h3>
-            <p>A delicious burger with ingredients:</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p><strong>Total price: {props.price.toFixed()}</strong></p>
-            <p>Continue to checkout</p>
-            <Button btnStyle="Danger" clicked={props.purchaseCanceled}>CANCEL</Button>
-            <Button btnStyle="Success" clicked={props.purchaseContinue}>CONTINUE</Button>
-        </Fragment>
-    );
+class OrderSummary extends Component {
+    render() {
+        const ingredientSummary = Object.keys(this.props.ingredients)
+            .map(igKey => {
+                return (
+                    <li key={igKey}>
+                        <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}
+                    </li>
+                )
+            })
+        return (
+            <Fragment>
+                <h3>Your Order</h3>
+                <p>A delicious burger with ingredients:</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p><strong>Total price: {this.props.price.toFixed()}</strong></p>
+                <p>Continue to checkout</p>
+                <Button btnStyle="Danger" clicked={this.props.purchaseCanceled}>CANCEL</Button>
+                <Button btnStyle="Success" clicked={this.props.purchaseContinue}>CONTINUE</Button>
+            </Fragment>
+        );
+    }
 }
-export default orderSummary;
+
+export default OrderSummary;
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,19 +1,30 @@
-import React, {Fragment} from "react";
+import React, {Component, Fragment} from "react";
 import classes from "./Modal.module.css";
 import Backdrop from "../Backdrop/Backdrop";
 
-const modal = (props) => (
-    <Fragment>
-        <Backdrop show={props.show} clicked={props.modalClosed}/>
-        <div
-            className={classes.Modal}
-            style={{
-                transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                opacity: props.show ? '1' : '0',
-            }}
-        >
-            {props.children}
-        </div>
-    </Fragment>
-)
-export default modal;
+class Modal extends Component {
+    shouldComponentUpdate(nextProps, nextState, nextContext) {
+        // tweak omdat modal enkel niet altijd zichtbaar is en dus maw niet moet geupdate worden
+        return nextProps.show !== this.props.show;
+    }
+
+    render() {
+        return (
+            <Fragment>
+                <Backdrop show={this.props.show} clicked={this.props.modalClosed}/>
+                <div
+                    className={classes.Modal}
+                    style={{
+                        transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                        opacity: this.props.show ? '1' : '0',
+                    }}
+                >
+                    {this.props.children}
+                </div>
+            </Fragment>
+        )
+    }
+}
+
+
+export default Modal;
