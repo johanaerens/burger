95c222fbd2787f89b87d4819e66c06ea2025bb54
Johan Aerens <johan.aerens@securex.be>
2021-06-02 17:13:37 +0200

    sidedrawer & toolbar

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,10 @@
+import React from "react";
+import burgerLogo from "../../assest/images/burger-logo.png"
+import classes from "./Logo.module.css";
+
+const logo = (props) => (
+    <div className={classes.Logo}>
+        <img src={burgerLogo} alt="MyBurger"/>
+    </div>
+)
+export default logo;
